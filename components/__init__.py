72e611e78b1c85696c5732f1e619e05d902d0edf
jethin-techie <jethin050607@gmail.com>
2025-11-17 20:51:47 +0530

    Components

---
+++ components/__init__.py
@@ -0,0 +1,6 @@
+from .battery import Battery
+from .motor import Motor, PMSM_Motor
+from .inverter import Inverter
+from .transmission import Transmission
+
+__all__ = ["Battery", "Motor", "PMSM_Motor", "Inverter", "Transmission"]
